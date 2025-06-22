2bacb500dfa6125fc944c64bebbffb8c6716a257
anudeepkumar9347 <137191853+anudeepkumar9347@users.noreply.github.com>
2025-06-22 17:03:30 +0530

    testing code scanning automation

--- insecure.js
+++ insecure.js
@@ -5,6 +5,6 @@ app.get('/user/:id', function (req, res) {
   // ❌ Insecure: unsanitized user input in dynamic code
   eval("console.log('User ID: " + req.params.id + "')");
   res.send('User page');
-});
+}); 
 
 app.listen(3000);
