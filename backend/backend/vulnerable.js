25ced309ea6bce4796ff9cc01e5e08315b67ff70
anudeepkumar9347 <137191853+anudeepkumar9347@users.noreply.github.com>
2025-06-22 16:59:23 +0530

    testing code scanning automation

---
+++ backend/backend/vulnerable.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// ❌ Insecure use of eval (should trigger CodeQL alert)
+app.get('/vuln/:data', (req, res) => {
+  eval("console.log('Data: ' + '" + req.params.data + "')");
+  res.send('Evaluated!');
+});
+
+app.listen(3000);
+
