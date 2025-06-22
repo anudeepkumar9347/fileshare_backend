b6b8a3e8693c0a3382d97729922d9ee5bfa8c222
anudeepkumar9347 <137191853+anudeepkumar9347@users.noreply.github.com>
2025-06-22 17:31:09 +0530

    Update vulnerable.js

--- backend/backend/vulnerable.js
+++ backend/backend/vulnerable.js
@@ -1,14 +1,4 @@
-   const express = require('express');
-const app = express();
 
-// ❌ Insecure use of eval (should trigger CodeQL alert)
-app.get('/vuln/:data', (req, res) => {
-  eval("console.log('Data: ' + '" + req.params.data + "')");
-  res.send('Evaluated!');
-});
-
-app.listen(3000);
- 
  
 // backend/vulnerable.js
 const input = require('fs').readFileSync('/dev/stdin').toString();
